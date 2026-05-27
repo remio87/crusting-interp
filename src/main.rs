75930b61df01100b77c8ac5c9317ba9c6a85d043
remio87 <82135212+remio87@users.noreply.github.com>
2026-05-27 20:56:02 +0900

    implementing dummy run* functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::env;
+use std::fs;
+use std::io::Write;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 2 {
+        eprintln!("Usage: rlox: [script]");
+    } else if args.len() == 2 {
+        run_file(Path::new(&args[1]));
+    } else {
+        run_prompt();
+    }
+}
+
+fn run_file(path: &Path) {
+    let contents = fs::read_to_string(path).expect("Should have been able to read the file");
+    dbg!(&contents);
+    run(contents);
+}
+
+fn run_prompt() {
+    loop {
+        let mut buf = String::new();
+        print!("> ");
+        std::io::stdout().flush().expect("failed to flush stdout");
+        std::io::stdin()
+            .read_line(&mut buf)
+            .expect("failed to read stdin");
+        println!("{}", buf.trim());
+    }
+}
+
+fn run(code: String) {
+    todo!();
 }
